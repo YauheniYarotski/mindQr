e6dd85444b67a54a71a22538bc09f21f349867f3
Yauheni Yarotski <yauheni_yarotski@me.com>
2017-06-11 15:43:51 +0300

    created empty project

---
+++ MindQr/LaunchVC.swift
@@ -0,0 +1,19 @@
+//
+//  LaunchVC.swift
+//  MindQr
+//
+//  Created by Yauheni Yarotski on 6/11/17.
+//  Copyright © 2017 Yauheni Yarotski. All rights reserved.
+//
+
+import UIKit
+
+class LaunchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
